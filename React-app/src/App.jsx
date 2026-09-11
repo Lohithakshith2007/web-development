ec0e215e598994f48ab78b76110d580aba95f7a4
lohithakshith <lohithakshith2007@gmail.com>
2026-09-11 21:15:10 +0530

    CSS: add CSS flexbox container and item properties example

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
     {/* <UserCard/> */}
 
     {/* student cards exercise */}
-    <StudentData name="Lohith" age={21} isStudent={true} major="Computer Science" cgpa={9.5}/>
+    {/* <StudentData name="Lohith" age={21} isStudent={true} major="Computer Science" cgpa={9.5}/>
     <StudentData name="sreesanth" age={22} isStudent={false} major="Mathematics" cgpa={8.2}/>
     <StudentData name="akshay" age={20} isStudent={true} major="Physics" cgpa={9.0}/>
-    <StudentData name="karthik" age={23} isStudent={false} major="Chemistry" cgpa={7.8}/>
+    <StudentData name="karthik" age={23} isStudent={false} major="Chemistry" cgpa={7.8}/> */}
     </>
 
   );
